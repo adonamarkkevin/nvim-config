ef4170fd032ed0871de4f39db1e6a524e26248da
Mark Kevin Adona <adonamarkkevin@gmail.com>
2024-08-11 18:33:13 +0800

    feat: vim quit with 2 C-c

--- lua/remap.lua
+++ lua/remap.lua
@@ -20,4 +20,20 @@ vim.api.nvim_set_keymap('i', '<C-c>', '<Esc>', { noremap = true })
 vim.api.nvim_set_keymap('v', '<C-c>', '<Esc>', { noremap = true })
 
 -- remove highlight
-vim.api.nvim_set_keymap('n', '<Esc>', ':noh<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<Esc>', ':noh<CR>', { noremap = true, silent = true })
+
+-- exit vim with 2 ctrl-c
+local ctrl_c_pressed = 0
+
+function HandleCtrlCExit()
+    if ctrl_c_pressed == 0 then
+        ctrl_c_pressed = 1
+        vim.cmd('echo "Press <C-c> again to exit, or any other key to cancel."')
+        return
+    end
+
+    ctrl_c_pressed = 0
+    vim.cmd('confirm quit')
+end
+
+vim.api.nvim_set_keymap('n', '<C-c>', ':lua HandleCtrlCExit()<CR>', { noremap = true, silent = true })
