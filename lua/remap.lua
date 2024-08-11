87d1ede6acce18598798f5cd8510fdb1d1d2b162
Mark Kevin Adona <adonamarkkevin@gmail.com>
2024-08-11 17:56:24 +0800

    feat: esc noh, C-c exit insert/visual

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,11 +1,23 @@
-vim.g.mapleader = " " --leader key
+-- leader key
+vim.g.mapleader = " "
 
+-- tab width
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
-vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true }) -- save using ctrl + s
+-- save using ctrl + s
+vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
 
 -- toggle relative line numbers
 vim.api.nvim_set_keymap('n', '<leader>rn', ':set relativenumber!<CR>', { noremap = true, silent = true })
+
+-- exit insert
+vim.api.nvim_set_keymap('i', '<C-c>', '<Esc>', { noremap = true })
+
+-- exit visual
+vim.api.nvim_set_keymap('v', '<C-c>', '<Esc>', { noremap = true })
+
+-- remove highlight
+vim.api.nvim_set_keymap('n', '<Esc>', ':noh<CR>', {noremap = true, silent = true})
