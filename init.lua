8f402e8ee7791a478dd3187c2077f64550bb32b3
Mark Kevin Adona <adonamarkkevin@gmail.com>
2024-08-19 22:03:16 +0800

    feat: make default tab width to 2

--- init.lua
+++ init.lua
@@ -13,8 +13,8 @@ vim.opt.rtp:prepend(lazypath)
 
 
 -- Set Neovim tab settings
-vim.opt.tabstop = 4      -- Number of spaces that a <Tab> in the file counts for
-vim.opt.shiftwidth = 4   -- Number of spaces to use for each step of (auto)indent
+vim.opt.tabstop = 2      -- Number of spaces that a <Tab> in the file counts for
+vim.opt.shiftwidth = 2   -- Number of spaces to use for each step of (auto)indent
 vim.opt.expandtab = true -- Convert tabs to spaces
 
 
