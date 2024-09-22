c751ad59c6fcb87e418b403fb7b95948e50a6324
Mark Kevin Adona <adonamarkkevin@gmail.com>
2024-09-22 10:18:34 +0800

    feat: clone neotree by pressing c-n again

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -10,16 +10,34 @@ return {
         require("neo-tree").setup({
             filesystem = {
                 filtered_items = {
-                    hide_dotfiles = false,              -- show dotfiles
-                    hide_hidden = false,                -- show hidden files
+                    hide_dotfiles = false, -- show dotfiles
+                    hide_hidden = false, -- show hidden files
                 },
                 follow_current_file = { enabled = true }, -- expand and focus on current file
-                use_libuv_file_watcher = true           -- enables file system watcher
+                use_libuv_file_watcher = true, -- enables file system watcher
             },
         })
 
-        -- Key mappings for Neo-tree
-        vim.keymap.set("n", "<c-n>", ":Neotree filesystem reveal left<cr>", {})
+        -- Key mappings for Neo-tree with toggle functionality
+        vim.keymap.set("n", "<c-n>", function()
+            -- Check if Neo-tree is currently open by looking for any window with "neo-tree"
+            local neo_tree_open = false
+            for _, win in ipairs(vim.api.nvim_list_wins()) do
+                local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(win))
+                if bufname:match("neo%-tree") then
+                    neo_tree_open = true
+                    break
+                end
+            end
+
+            if neo_tree_open then
+                vim.cmd("Neotree close") -- Close Neo-tree if it's open
+            else
+                vim.cmd("Neotree filesystem reveal left") -- Open Neo-tree if it's not open
+            end
+        end, { noremap = true, silent = true })
+
+        -- Key mapping for buffers in float mode
         vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<cr>", {})
     end,
 }
