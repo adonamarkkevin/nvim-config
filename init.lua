5d241aadf2b8b3d5f1715eb65a2ee977b41cf57d
Mark Kevin Adona <adonamarkkevin@gmail.com>
2025-04-25 04:59:20 +0800

    feat: diagnostic for lsp error

--- init.lua
+++ init.lua
@@ -23,5 +23,36 @@ vim.opt.linebreak = true -- Wrap lines at convenient points (like spaces)
 vim.opt.formatoptions:append("t") -- Automatically wrap text when typing beyond 80 characters
 vim.opt.colorcolumn = "80" -- Display a vertical line at the 80-character mark
 
+-- Diagnostic error width max at 80
+vim.diagnostic.config({
+    virtual_text = {
+        source = "if_many",
+        format = function(diagnostic)
+            local max_len = 60
+            local msg = diagnostic.message
+            if #msg > max_len then
+                return msg:sub(1, max_len) .. "…"
+            end
+            return msg
+        end,
+    },
+    float = {
+        source = "always",
+        border = "rounded",
+        max_width = 100,
+        focusable = true,
+    },
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+})
+
+-- Diagnostic hover error display
+vim.o.updatetime = 250
+vim.cmd([[
+  autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})
+]])
+
 require("remap")
 require("lazy").setup("plugins")
