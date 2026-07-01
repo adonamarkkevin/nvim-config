18ceff6cbf819ba500c73499c1fede8ae86bb2ee
Mark Kevin Adona <adonamarkkevin@gmail.com>
2026-07-02 01:15:21 +0800

    fix(nvim): remove treesitter foldexpr causing auto-collapsed folds

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -44,8 +44,6 @@ return {
                 pattern = filetypes,
                 callback = function()
                     vim.treesitter.start()
-                    vim.wo[0][0].foldmethod = "expr"
-                    vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
                     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
                 end,
             })
