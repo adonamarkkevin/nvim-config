2258ba5b1f8c61a929d23b90f7ed36feec4a6888
Mark Kevin Adona <adonamarkkevin@gmail.com>
2026-07-02 00:59:54 +0800

    fix(nvim): migrate nvim-treesitter to main branch for Neovim 0.12 compat

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,33 +1,53 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        branch = "main",
+        lazy = false,
         build = ":TSUpdate",
         config = function()
-            local config = require("nvim-treesitter.configs")
-            config.setup({
-                ensure_installed = {
-                    "lua",
-                    "markdown",
-                    "markdown_inline",
-                    "go",
-                    "gomod",
-                    "gosum",
-                    "gowork",
-                    "html",
-                    "json",
-                    "jsdoc",
-                    "tsx",
-                    "typescript",
-                    "javascript",
-                    "graphql",
-                    "sql",
-                },
-                auto_install = true,
-                highlight = {
-                    enable = true,
-                    disable = { "markdown", "markdown_inline" },
-                },
-                indent = { enable = true },
+            local parsers = {
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "go",
+                "gomod",
+                "gosum",
+                "gowork",
+                "html",
+                "json",
+                "jsdoc",
+                "tsx",
+                "typescript",
+                "javascript",
+                "graphql",
+                "sql",
+            }
+
+            require("nvim-treesitter").install(parsers)
+
+            local filetypes = {
+                "lua",
+                "go",
+                "gomod",
+                "gosum",
+                "gowork",
+                "html",
+                "json",
+                "typescriptreact",
+                "typescript",
+                "javascript",
+                "graphql",
+                "sql",
+            }
+
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = filetypes,
+                callback = function()
+                    vim.treesitter.start()
+                    vim.wo[0][0].foldmethod = "expr"
+                    vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end,
             })
         end,
     },
